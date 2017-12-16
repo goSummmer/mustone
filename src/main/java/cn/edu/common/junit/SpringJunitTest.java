f25c1105fecbdf382a49e9c05dff25a93bf0c66e
YaoRui <30330466+goSummmer@users.noreply.github.com>
2017-12-16 23:55:25 +0800

    Update SpringJunitTest.java

--- src/main/java/cn/edu/common/junit/SpringJunitTest.java
+++ src/main/java/cn/edu/common/junit/SpringJunitTest.java
@@ -1,12 +1,10 @@
 package cn.edu.common.junit;
-/**
- * 测试公共类
- */
+
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)   ///这时spring结合junit
+@RunWith(SpringJUnit4ClassRunner.class)   
 @ContextConfiguration(locations="classpath:application-context.xml")
 public class SpringJunitTest {
 
